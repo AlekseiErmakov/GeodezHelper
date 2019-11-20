90972b2bb00788a786b4e9a79a0cf6153e40305b
lex_laeda <lex_laeda@mail.ru>
2019-11-20 22:44:34 +0300

    add writer

--- app/src/main/java/com/example/geodezhelper/GeodezFileReader.java
+++ app/src/main/java/com/example/geodezhelper/GeodezFileReader.java
@@ -1,5 +1,33 @@
 package com.example.geodezhelper;
 
-public class GeodezFileReader {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class GeodezFileReader {
+        BufferedReader reader;
+        String filename;
+        ArrayList<String> Fromfile;
+        public GeodezFileReader(String filename){
+            this.filename = filename;
+            Fromfile = new ArrayList<>();
+        }
+        public void read(){
+            try {
+                reader =new BufferedReader(new FileReader(filename));
+                while (reader.ready()){
+                    String s =reader.readLine();
+                    Fromfile.add(s);
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public ArrayList<String>getFromfile(){
+            return Fromfile;
+        }
 }
